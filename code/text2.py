d5c24c7bfa35ede2814412b0c7b0eadebbfa5951
Marko Prelevikj <pr3mar@users.noreply.github.com>
2019-12-28 22:34:37 +0100

    Tokenizing into sentences and LASER embeddings

--- code/text2.py
+++ code/text2.py
@@ -1,4 +1,16 @@
 import numpy as np
+import requests
+import nltk
+tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
 
 if __name__ == '__main__':
-    print("Hello World!")
+    url = "http://127.0.0.1:8080/vectorize"
+    text = "Hey, how are you? I'm OK and you? This is another sentence."
+    sentences = tokenizer.tokenize(text)
+    print(sentences)
+    for s in sentences:
+        params = {"q": s, "lang": "en"}
+        resp = np.array(requests.get(url=url, params=params).json()["embedding"])
+        print(resp.shape)
+        print(resp)
+
