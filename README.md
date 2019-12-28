a7b92ea3aee843b44e54b1a1df8a475f7f49806e
Marko Prelevikj <pr3mar@users.noreply.github.com>
2019-12-28 22:57:40 +0100

    Writing README and initial preprocessing

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # tda-text2
+
+Link to the [data set](https://drive.google.com/uc?id=17WBziFbt9nhAW5iV-yHPHmCfquBPrjJO&export=download)
+
+Setting up:
+
+```bash
+pipenv install
+pipenv run <insert command here> # see https://realpython.com/pipenv-guide/ for more
+```
+
+Dependency is [LASER](https://github.com/facebookresearch/LASER). Follow the instructions [here](https://github.com/facebookresearch/LASER/tree/master/docker) to setup the API via docker. 
--- code/text2.py
+++ code/text2.py
@@ -1,11 +1,13 @@
 import numpy as np
 import requests
 import nltk
+# nltk.download('punkt')  # run this line the first time only
 tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
+port = 8080  # the same as from the docker command
 
 if __name__ == '__main__':
-    url = "http://127.0.0.1:8080/vectorize"
-    text = "Hey, how are you? I'm OK and you? This is another sentence."
+    url = f"http://127.0.0.1:{port}/vectorize"
+    text = "Hey, how are you? I'm OK and you?! this is \"another sentence\""
     sentences = tokenizer.tokenize(text)
     print(sentences)
     for s in sentences:
