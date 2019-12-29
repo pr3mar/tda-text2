cc338d0a25bf7c18e6400143f6e1b34d1ae9f202
Marko Prelevikj <pr3mar@users.noreply.github.com>
2019-12-29 22:54:56 +0100

    We can now build matrices, hopefully

--- README.md
+++ README.md
@@ -9,4 +9,25 @@ pipenv install
 pipenv run <insert command here> # see https://realpython.com/pipenv-guide/ for more
 ```
 
-Dependency is [LASER](https://github.com/facebookresearch/LASER). Follow the instructions [here](https://github.com/facebookresearch/LASER/tree/master/docker) to setup the API via docker. 
+Dependency is [LASER](https://github.com/facebookresearch/LASER). Follow the instructions [here](https://github.com/facebookresearch/LASER/tree/master/docker) to setup the API via docker.
+
+---
+## Categories 
+
+Children's novels:
+- Thornton Waldo Burgess (31)
+- Ballantyne, R. M. (88)
+
+Detective Fiction:
+- Sir Arthur Conan Doyle (57)
+
+Adventure Fiction:
+- Mark Twain (48)
+- Jack London (48)
+
+Realistic novels:
+- Charles Dickens (61)
+
+Non-fiction:
+- Charles Darwin (20)
+- Abraham Lincoln (16)
--- code/text2.py
+++ code/text2.py
@@ -1,16 +1,47 @@
 import numpy as np
-import requests
-import nltk
+import json
+import re
+from os import listdir
+from os.path import isfile, join
 from laserembeddings import Laser
+from time import time
+import nltk
 # nltk.download('punkt')  # run this line the first time only
 
 tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
 laser = Laser()
+DATASET_BASE = "../dataset/"
+
+
+
+def get_genre_matrix(genre, authors):
+    onlyfiles = [f for f in listdir(DATASET_BASE) if isfile(join(DATASET_BASE, f))]
+    current = [x for x in onlyfiles if any(x.startswith(a) for a in authors)]
+    M = np.zeros((len(current), 1024))
+    for i, book in enumerate(current):
+        M[i, :] = get_file_matrix(book)
+    np.save(f"../cache/{genre}", M)
+    return M
+
+
+def get_file_matrix(fname):
+    with open(f"{DATASET_BASE}{fname}") as file:
+        sentences = tokenizer.tokenize(re.sub(r"(\s|\n)+", " ", file.read()))
+        return np.mean(laser.embed_sentences(sentences, lang="en"), axis=0)
+
 
 if __name__ == '__main__':
-    text = "Hey, how are you? I'm OK and you?! this is \"another sentence\". Woo hooo"
-    sentences = tokenizer.tokenize(text)
-    print(sentences)
-    embeddings = laser.embed_sentences(sentences, lang='en')
-    print(embeddings.shape)
-    print(embeddings)
+    # embeddings = get_file_matrix('../dataset/Thornton Waldo Burgess___Whitefoot the Wood Mouse.txt')
+    # print(embeddings.shape)
+    data = json.load(open("../data_categories.json"))
+    matrices = []
+    for genre in data:
+        begin = time()
+        matrices.append(get_genre_matrix(genre, data[genre]))
+        print(f"Time for processing: {time() - begin}")
+    # text = "Hey, how are you? I'm OK and you?! this is \"another sentence\". Woo hooo"
+    # sentences = tokenizer.tokenize(text)
+    # print(sentences)
+    # embeddings = laser.embed_sentences(sentences, lang='en')
+    # print(embeddings.shape)
+    # print(embeddings)
