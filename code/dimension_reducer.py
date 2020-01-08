0077fe5695006319f74c49367ee3a5f73a4a546b
BStevanoski <stevanoski.bozidar@gmail.com>
2020-01-09 00:07:24 +0100

    Added explanation for SVD.

--- code/dimension_reducer.py
+++ code/dimension_reducer.py
@@ -8,5 +8,6 @@ for file_name in os.listdir(DATA_PATH + 'full_dimension/'):
     if file_name.endswith('.npy'):
         matrix = np.load(DATA_PATH + 'full_dimension/' + file_name)
         u, s, vh = np.linalg.svd(matrix)
-        reduced = vh[:r, :]
+        reduced = vh[:r, :] # there is a difference in notation between instruction PDF and Numpy - the matrix V is not
+        # transposed in numpy -> see first sentence of https://docs.scipy.org/doc/numpy/reference/generated/numpy.linalg.svd.html
         np.save(DATA_PATH + 'reduced_dimension/' + file_name, reduced)
