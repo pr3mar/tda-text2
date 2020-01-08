dc6e7848dd9985afbfc4067ba4e43e3130a565bd
BStevanoski <stevanoski.bozidar@gmail.com>
2020-01-08 23:38:46 +0100

    Refactored dimension reduction.

--- code/dimension_reducer.py
+++ code/dimension_reducer.py
@@ -4,9 +4,9 @@ import os
 DATA_PATH = '../cache/all_words/'
 r = 5
 
-for file_name in os.listdir(DATA_PATH+'full_dimension/'):
+for file_name in os.listdir(DATA_PATH + 'full_dimension/'):
     if file_name.endswith('.npy'):
-        matrix = np.load(DATA_PATH+'full_dimension/'+file_name)
+        matrix = np.load(DATA_PATH + 'full_dimension/' + file_name)
         u, s, vh = np.linalg.svd(matrix)
         reduced = vh[:r, :]
-        np.save(DATA_PATH+'reduced_dimension/' + file_name, reduced)
+        np.save(DATA_PATH + 'reduced_dimension/' + file_name, reduced)
