c57b31e7057318020a6e20f0bb046add02ef6b8c
BStevanoski <stevanoski.bozidar@gmail.com>
2020-01-08 23:28:41 +0100

    Added dimension reduction.

---
+++ code/dimension_reducer.py
@@ -0,0 +1,12 @@
+import numpy as np
+import os
+
+DATA_PATH = '../cache/all_words/'
+r = 5
+
+for file_name in os.listdir(DATA_PATH+'full_dimension/'):
+    if file_name.endswith('.npy'):
+        matrix = np.load(DATA_PATH+'full_dimension/'+file_name)
+        u, s, vh = np.linalg.svd(matrix)
+        reduced = vh[:r, :]
+        np.save(DATA_PATH+'reduced_dimension/' + file_name, reduced)
